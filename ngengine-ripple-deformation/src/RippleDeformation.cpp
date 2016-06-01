2e7612ee4ca4d3404e054d25a18c0e511ba7628a
Tymoteusz Kubicz <tymoteusz.kubicz@gmail.com>
2016-06-01 17:32:22 +0200

    Ripple deformation example almost finished.

--- ngengine-ripple-deformation/src/RippleDeformation.cpp
+++ ngengine-ripple-deformation/src/RippleDeformation.cpp
@@ -16,30 +16,115 @@ bool RippleDeformation::Init() {
 
 	check_gl_error();
 
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_CULL_FACE);
+	CreateTopology();
+	CreateBufferObjects();
+
+	//glEnable(GL_DEPTH_TEST);
+	//glEnable(GL_CULL_FACE);
+	
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	return true;
 }
 
+void RippleDeformation::Prepare(float dt) {
+	time = dt * 1000;
+}
+
 void RippleDeformation::Render() {
-	Rendering::Renderer::GetInstance().ClearColor(0.9f, 0.95f, 1.0f, 1.0f);
+	Rendering::Renderer::GetInstance().ClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	Rendering::Renderer::GetInstance().ClearBuffers();
 	Rendering::Renderer::GetInstance().GetMatrixStack().Identity();
+
+	Math::mat4f T, Rx, MV, MVP;
+	T.Translate(0, 0, dist);
+	Rx = T;
+	Rx.RotateX(rX);
+	MV = Rx;
+	MV.RotateY(rY);
+	MVP = P * MV;
+
+	shader->bindShader();
+	shader->sendUniform4x4("MVP", &MVP[0], false);
+	shader->sendUniform("time", time);
+	glDrawElements(GL_TRIANGLES, TOTAL_INDICES, GL_UNSIGNED_SHORT, 0);
+	shader->unbindShader();
 }
 
 void RippleDeformation::Shutdown() {
+	shader->unload();
+
+	glDeleteBuffers(1, &vboVerticesID);
+	glDeleteBuffers(1, &vboIndicesID);
+	glDeleteVertexArrays(1, &vaoID);
 }
 
 void RippleDeformation::OnResize(int width, int height) {
 	Application::OnResize(width, height);
+
+	P.SetPerspectiveProjection(45.f, float(width) / float(height), 1.f, 1000.f);
 }
 
+void RippleDeformation::OnMouse(NGE::Events::MouseEvent& event) {
+}
+
+
 void RippleDeformation::CreateTopology() {
-	//	int count = 0, i = 0, j = 0;
-	//	for (j = 0; j < NUM_Z; ++j) {
-	//		for (i = 0; i < NUM_X; ++i) {
-	//			vertices[count++] = Math::vec3f()
-	//		}
-	//	}
+	// Setup plane geometry.
+	int count = 0, i = 0, j = 0;
+	for (j = 0; j < NUM_Z; ++j) {
+		for (i = 0; i < NUM_X; ++i) {
+			vertices[count++] = Math::vec3f(((float(i) / (NUM_X - 1)) * 2 - 1) * HALF_SIZE_X, 0, ((float(j) / (NUM_Z - 1)) * 2 - 1) * HALF_SIZE_Z);
+		}
+	}
+
+	// Fill plane indices array.
+	GLushort* id = &indices[0];
+	for (i = 0; i < NUM_Z; ++i) {
+		for (j = 0; j < NUM_X; ++j) {
+			int i0 = i * (NUM_X + 1) + j;
+			int i1 = i0 + 1;
+			int i2 = i0 + (NUM_X + 1);
+			int i3 = i2 + 1;
+			if ((j + i) % 2) {
+				*id++ = i0;
+				*id++ = i2;
+				*id++ = i1;
+				*id++ = i1;
+				*id++ = i2;
+				*id++ = i3;
+			} else {
+				*id++ = i0;
+				*id++ = i2;
+				*id++ = i3;
+				*id++ = i0;
+				*id++ = i3;
+				*id++ = i1;
+			}
+		}
+	}
+}
+
+void RippleDeformation::CreateBufferObjects() {
+	glGenVertexArrays(1, &vaoID);
+	glGenBuffers(1, &vboVerticesID);
+	glGenBuffers(1, &vboIndicesID);
+
+	glBindVertexArray(vaoID);
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, vboVerticesID);
+		glBufferData(GL_ARRAY_BUFFER, sizeof (vertices), &vertices[0], GL_STATIC_DRAW);
+
+		check_gl_error();
+
+		glEnableVertexAttribArray(shader->getAttribLocation("vVertex"));
+		glVertexAttribPointer(shader->getAttribLocation("vVertex"), 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+		check_gl_error();
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboIndicesID);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof (indices), &indices[0], GL_STATIC_DRAW);
+
+		check_gl_error();
+	}
 }
--- ngengine-ripple-deformation/src/RippleDeformation.h
+++ ngengine-ripple-deformation/src/RippleDeformation.h
@@ -10,6 +10,7 @@
 
 #include <NGE/Windows/Application.hpp>
 #include <NGE/Media/Shaders/GLSLProgram.hpp>
+#include <NGE/Math/Math.hpp>
 
 using namespace NGE;
 
@@ -22,7 +23,7 @@ class RippleDeformation : public NGE::Windows::Application {
 
 	bool Init() override;
 
-	void Prepare(float dt) override { }
+	void Prepare(float dt) override;
 	void Render() override;
 	void Shutdown() override;
 
@@ -30,18 +31,61 @@ class RippleDeformation : public NGE::Windows::Application {
 
 	void OnKeyPressed(NGE::Events::KeyboardEvent& event) override { }
 
-	void OnMouse(NGE::Events::MouseEvent& event) override { }
+	void OnMouse(NGE::Events::MouseEvent& event) override;
 
 	void OnMouseDrag(int x, int y) override { }
 
   private:
 	void CreateTopology();
+	void CreateBufferObjects();
 
   private:
+	/**
+	 * Total quads on x axis.
+	 */
+	static constexpr int NUM_X = 40;
+	/**
+	 * Total quads on z axis.
+	 */
+	static constexpr int NUM_Z = 40;
+
+	/**
+	 * Size of the plane in world space on x axis.
+	 */
+	static constexpr float SIZE_X = 4;
+
+	/**
+	 * Size of the plane in world space on z axis.
+	 */
+	static constexpr float SIZE_Z = 4;
+	static constexpr float HALF_SIZE_X = SIZE_X / 2.0f;
+	static constexpr float HALF_SIZE_Z = SIZE_Z / 2.0f;
+
+	/**
+	 * Ripple displacement speed
+	 */
+	static constexpr float SPEED = 2;
+
+	Math::vec3f vertices[(NUM_X + 1) * (NUM_Z + 1)];
+
+	static constexpr int TOTAL_INDICES = NUM_X * NUM_Z * 2 * 3;
+
+	GLushort indices[TOTAL_INDICES];
+
+	// Camera transformation variables.
+	int state = 0, oldX = 0, oldY = 0;
+	float rX = 25, rY = 40, dist = -7;
+
+	// Projection and modelview matrix.
+	Math::mat4f P;
+	Math::mat4f MV;
+
+	float time = 0;
+
 	Media::Shaders::GLSLProgram* shader;
 
-	GLuint vba;
-	GLuint vbo;
+	GLuint vaoID;
+	GLuint vboVerticesID, vboIndicesID;
 };
 
 #endif /* RIPPLEDEFORMATION_H */
