52b796379d2de469f944096d30dd46b248d1008e
Tymoteusz Kubicz <tymoteusz.kubicz@gmail.com>
2016-06-14 18:26:28 +0200

    Image loader example finished.

--- ngengine-simple-image-loader/src/SimpleImageLoader.cpp
+++ ngengine-simple-image-loader/src/SimpleImageLoader.cpp
@@ -5,16 +5,25 @@
 #include <NGE/Media/Images/TextureManager.hpp>
 #include <NGE/Media/Images/LoadTextureEvent.hpp>
 #include <NGE/Media/Images/GetTextureEvent.hpp>
+#include <NGE/Media/Shaders/ShaderManager.hpp>
+#include <NGE/Media/Shaders/LoadShaderEvent.hpp>
+#include <NGE/Media/Shaders/GetShaderEvent.hpp>
+#include <NGE/Tools/GLError.hpp>
 
 namespace e = NGE::Events;
 namespace i = NGE::Media::Images;
+namespace s = NGE::Media::Shaders;
 
 bool SimpleImageLoader::Init() {
 	e::EventManager& eventManager = e::EventManager::GetInstance();
 	i::TextureManager& textureManager = i::TextureManager::GetInstance();
+	s::ShaderManager& shaderManager = s::ShaderManager::GetInstance();
+
 	textureManager.Initialise();
+	shaderManager.Initialise();
 
 	std::string imageName = "Lenna";
+	std::string shaderName = "shader";
 
 	pugi::xml_document doc;
 	auto result = doc.load_file("config/image.xml");
@@ -24,24 +33,46 @@ bool SimpleImageLoader::Init() {
 	}
 	auto imageNode = doc.child("Texture2D");
 
-	auto loadEvent = std::make_shared<i::LoadTextureEvent>(imageNode);
-	auto getEvent = std::make_shared<i::GetTextureEvent>(imageName);
+	auto loadTextureEvent = std::make_shared<i::LoadTextureEvent>(imageNode);
+	auto getTextureEvent = std::make_shared<i::GetTextureEvent>(imageName);
+	auto loadShaderEvent = std::make_shared<s::LoadShaderEvent>(shaderName, "shader.xml");
+	auto getShaderEvent = std::make_shared<s::GetShaderEvent>(shaderName);
 
-	eventManager.TriggerEvent(loadEvent);
-	eventManager.TriggerEvent(getEvent);
+	eventManager.QueueEvent(loadTextureEvent);
+	eventManager.QueueEvent(loadShaderEvent);
 
-	if (!getEvent->IsSuccessful()) {
+	eventManager.QueueEvent(getTextureEvent);
+	eventManager.QueueEvent(getShaderEvent);
+
+	eventManager.UpdateAll();
+
+	if (!getTextureEvent->IsSuccessful()) {
 		log_error("Could not get '{}' image", imageName);
 		return false;
 	}
+
+	if (!getShaderEvent->IsSuccessful()) {
+		log_error("Could not get '{}' shader", shaderName);
+		return false;
+	}
+
+	shader = getShaderEvent->GetShader();
+	texture = getTextureEvent->GetTexture();
+
+	CreateGeometry();
+
+	texture->Activate(GL_TEXTURE0);
 
 	return true;
 }
 
 void SimpleImageLoader::Render() {
-	Rendering::Renderer::GetInstance().ClearColor(0.9f, 0.95f, 1.0f, 1.0f);
+	Rendering::Renderer::GetInstance().ClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	Rendering::Renderer::GetInstance().ClearBuffers();
-	Rendering::Renderer::GetInstance().GetMatrixStack().Identity();
+
+	shader->BindShader();
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
+	shader->UnbindShader();
 }
 
 void SimpleImageLoader::Shutdown() {
@@ -49,4 +80,37 @@ void SimpleImageLoader::Shutdown() {
 
 void SimpleImageLoader::OnResize(int width, int height) {
 	Application::OnResize(width, height);
+}
+
+void SimpleImageLoader::CreateGeometry() {
+	// Setup quad geometry
+	vertices[0] = Math::vec2f(0.f, 0.f);
+	vertices[1] = Math::vec2f(1.f, 0.f);
+	vertices[2] = Math::vec2f(1.f, 1.f);
+	vertices[3] = Math::vec2f(0.f, 1.f);
+
+	// Setup quad indices
+	GLushort* id = &indices[0];
+	*id++ = 0;
+	*id++ = 1;
+	*id++ = 2;
+	*id++ = 0;
+	*id++ = 2;
+	*id++ = 3;
+
+	glGenVertexArrays(1, &vaoId);
+	glGenBuffers(1, &vboVerticesId);
+	glGenBuffers(1, &vboIndicesId);
+
+	glBindVertexArray(vaoId);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vboVerticesId);
+	glBufferData(GL_ARRAY_BUFFER, sizeof (vertices), &vertices[0], GL_STATIC_DRAW);
+	glEnableVertexAttribArray(shader->GetAttribLocation("vVertex"));
+	glVertexAttribPointer(shader->GetAttribLocation("vVertex"), 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboIndicesId);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof (indices), &indices[0], GL_STATIC_DRAW);
+
+	check_gl_error();
 }
--- ngengine-simple-image-loader/src/SimpleImageLoader.h
+++ ngengine-simple-image-loader/src/SimpleImageLoader.h
@@ -9,6 +9,9 @@
 #define SIMPLEIMAGELOADER_H
 
 #include <NGE/Windows/Application.hpp>
+#include <NGE/Media/Images/Texture.hpp>
+#include <NGE/Media/Shaders/GLSLProgram.hpp>
+#include <NGE/Math/Math.hpp>
 
 using namespace NGE;
 
@@ -16,6 +19,7 @@ class SimpleImageLoader : public NGE::Windows::Application {
   public:
 
 	SimpleImageLoader() { }
+
 	~SimpleImageLoader() { }
 
 	bool Init() override;
@@ -27,8 +31,24 @@ class SimpleImageLoader : public NGE::Windows::Application {
 	virtual void OnResize(int width, int height) override;
 
 	void OnKeyPressed(NGE::Events::KeyboardEvent& event) override { }
+
 	void OnMouse(NGE::Events::MouseEvent& event) override { }
+
 	void OnMouseDrag(int x, int y) override { }
+
+  private:
+	void CreateGeometry();
+
+  private:
+	GLuint vaoId;
+	GLuint vboVerticesId;
+	GLuint vboIndicesId;
+
+	Media::Shaders::GLSLProgram* shader;
+	Media::Images::Texture* texture;
+
+	Math::vec2f vertices[4];
+	GLushort indices[6];
 };
 
 #endif /* SIMPLEIMAGELOADER_H */
