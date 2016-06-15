b0cb8c067795efb305e701da7fdadbe2fbe8676e
Tymoteusz Kubicz <tymoteusz.kubicz@gmail.com>
2016-06-15 18:58:10 +0200

    Disable building ngengine tests for example build by default. Warning from ngengine-waterexample resolved.

--- ngengine-waterexample/src/Background.cpp
+++ ngengine-waterexample/src/Background.cpp
@@ -10,71 +10,76 @@
 #include <NGE/Events/EventManager.hpp>
 
 bool Background::Initialize(int slices, float radius) {
-    // Load background shader.
-    //NGE::Media::MediaManager::GetInstance().GetShaderManager().LoadProgram("backgroundShader", "background.xml");
-    auto loadShaderEvent = std::make_shared<NGE::Media::Shaders::LoadShaderEvent>("backgroundShader", "background.xml");
-    NGE::Events::EventManager::GetInstance().TriggerEvent(loadShaderEvent);
+	// Load background shader.
+	//NGE::Media::MediaManager::GetInstance().GetShaderManager().LoadProgram("backgroundShader", "background.xml");
+	auto loadShaderEvent = std::make_shared<NGE::Media::Shaders::LoadShaderEvent>("backgroundShader", "background.xml");
+	NGE::Events::EventManager::GetInstance().TriggerEvent(loadShaderEvent);
 
-    // Initialise the sphere.
-    auto getShaderEvent = std::make_shared<NGE::Media::Shaders::GetShaderEvent>("backgroundShader");
-    NGE::Events::EventManager::GetInstance().TriggerEvent(getShaderEvent);
-    NGE::Geometry::Basic::Sphere::Initialize(slices, radius, getShaderEvent->GetShader());
-    //NGE::Geometry::Basic::Sphere::Initialize(slices, radius, NGE::Media::MediaManager::GetInstance().GetShaderManager().GetProgram("backgroundShader"));
+	// Initialise the sphere.
+	auto getShaderEvent = std::make_shared<NGE::Media::Shaders::GetShaderEvent>("backgroundShader");
+	NGE::Events::EventManager::GetInstance().TriggerEvent(getShaderEvent);
+	NGE::Geometry::Basic::Sphere::Initialize(slices, radius, getShaderEvent->GetShader());
+	//NGE::Geometry::Basic::Sphere::Initialize(slices, radius, NGE::Media::MediaManager::GetInstance().GetShaderManager().GetProgram("backgroundShader"));
 
-    // Create the background cubemap from XML description file.
-    pugi::xml_document doc;
-    pugi::xml_parse_result result = doc.load_file("config/backgroundCubemap.xml");
-    pugi::xml_node cubeMap = doc.child("TextureCubeMap");
+	// Create the background cubemap from XML description file.
+	pugi::xml_document doc;
+	pugi::xml_parse_result result = doc.load_file("config/backgroundCubemap.xml");
+	if (!result) {
+		log_error("Could not read backgroundCubemap.xml");
+		return false;
+	}
 
-    auto loadTextureEvent = std::make_shared<NGE::Media::Images::LoadTextureEvent>(cubeMap);
-    NGE::Events::EventManager::GetInstance().TriggerEvent(loadTextureEvent);
-    log_debug("LoadTextureEvent was successful: {}", loadTextureEvent->IsSuccessful());
-    log_debug("Texture count: {}", NGE::Media::Images::TextureManager::GetInstance().GetTextureCount());
+	pugi::xml_node cubeMap = doc.child("TextureCubeMap");
+
+	auto loadTextureEvent = std::make_shared<NGE::Media::Images::LoadTextureEvent>(cubeMap);
+	NGE::Events::EventManager::GetInstance().TriggerEvent(loadTextureEvent);
+	log_debug("LoadTextureEvent was successful: {}", loadTextureEvent->IsSuccessful());
+	log_debug("Texture count: {}", NGE::Media::Images::TextureManager::GetInstance().GetTextureCount());
 
 
-    std::shared_ptr<NGE::Media::Images::GetTextureEvent> getTextureEvent = std::make_shared<NGE::Media::Images::GetTextureEvent>("backgroundCubemap");
-    NGE::Events::EventManager::GetInstance().TriggerEvent(getTextureEvent);
-    log_debug("GetTextureEvent was successful: {}", getTextureEvent->IsSuccessful());
-    log_debug("Texture count: {}", NGE::Media::MediaManager::GetInstance().GetTextureManager().GetTextureCount());
+	std::shared_ptr<NGE::Media::Images::GetTextureEvent> getTextureEvent = std::make_shared<NGE::Media::Images::GetTextureEvent>("backgroundCubemap");
+	NGE::Events::EventManager::GetInstance().TriggerEvent(getTextureEvent);
+	log_debug("GetTextureEvent was successful: {}", getTextureEvent->IsSuccessful());
+	log_debug("Texture count: {}", NGE::Media::MediaManager::GetInstance().GetTextureManager().GetTextureCount());
 
-    this->texture = getTextureEvent->GetTexture();
+	this->texture = getTextureEvent->GetTexture();
 
-    //Bind cubemap to the shader.
-    shader->BindShader();
-    {
-        texture->Activate(0);
-        shader->sendUniform("u_cubemap", 0);
-    }
-    shader->UnbindShader();
+	//Bind cubemap to the shader.
+	shader->BindShader();
+	{
+		texture->Activate(0);
+		shader->sendUniform("u_cubemap", 0);
+	}
+	shader->UnbindShader();
 
-    return true;
+	return true;
 }
 
 void Background::Render() {
-    // Clear color and depth buffers.
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	// Clear color and depth buffers.
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // Set visibility to the back of the sphere. As we use sphere as a skydome,
-    // we want to see the back of it.
-    glFrontFace(GL_CW);
+	// Set visibility to the back of the sphere. As we use sphere as a skydome,
+	// we want to see the back of it.
+	glFrontFace(GL_CW);
 
-    shader->BindShader();
-    {
-        // Send modelview and projection matrix to the shader.
-        shader->sendUniform4x4("modelviewMatrix", NGE::Rendering::Renderer::GetInstance().GetMatrixStack().GetMatrix(NGE::MODELVIEW_MATRIX));
-        shader->sendUniform4x4("projectionMatrix", NGE::Rendering::Renderer::GetInstance().GetMatrixStack().GetMatrix(NGE::PROJECTION_MATRIX));
+	shader->BindShader();
+	{
+		// Send modelview and projection matrix to the shader.
+		shader->sendUniform4x4("modelviewMatrix", NGE::Rendering::Renderer::GetInstance().GetMatrixStack().GetMatrix(NGE::MODELVIEW_MATRIX));
+		shader->sendUniform4x4("projectionMatrix", NGE::Rendering::Renderer::GetInstance().GetMatrixStack().GetMatrix(NGE::PROJECTION_MATRIX));
 
-        //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-        // Render the sphere.
-        glBindVertexArray(vertexArray);
-        {
-            glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
-        }
-        glBindVertexArray(0);
+		// Render the sphere.
+		glBindVertexArray(vertexArray);
+		{
+			glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+		}
+		glBindVertexArray(0);
 
-        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-    }
-    shader->UnbindShader();
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	}
+	shader->UnbindShader();
 }
 
